ff8b3823e84d155fbf1b33275bd5bcc3e3841d59
gpd <guopeidong@idcool.com.cn>
2017-08-16 11:22:15 +0800

    finish connect version

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const Port = 8124;
+
 let http = require('http');
+let path = require('path');
+let connect = require('connect');
 let htutil = require('./htutil');
+let favicon = require('serve-favicon');
+let morgan = require('morgan');
 
-let server = http.createServer(function(req, res) {
-	htutil.loadParams(req, res, undefined);
+let home = require('./views/home-node');
+let square = require('./views/square-node');
+let factorial = require('./views/factorial-node');
+let fibonacci = require('./views/fibo-node');
+let fibonacci2 = require('./views/fibo2-node');
+let mult = require('./views/mult-node');
 
-	if (req.requrl.pathname === "/") {
-		require('./views/home-node').get(req, res);
-	} else if (req.requrl.pathname === '/square') {
-		require('./views/square-node').get(req, res);
-	} else if (req.requrl.pathname === '/factorial') {
-		require('./views/factorial-node').get(req, res);
-	} else if (req.requrl.pathname === '/fibonacci') {
-		require('./views/fibo-node').get(req, res);
-	} else if (req.requrl.pathname === '/fibonacci2') {
-		require('./views/fibo2-node').get(req, res);
-	} else if (req.requrl.pathname === '/mult') {
-		require('./views/mult-node').get(req, res);
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'text/plain'
-		});
-		res.end("bad URL " + req.url);
+let app = connect();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+	.use(morgan('combined'));
+
+
+app.use(htutil.loadParams);
+app.use('/', function(req, res, next) {
+	if (req.url == "/") {
+		home.get(req, res);
 	}
-})
+	next();
+});
+app.use('/mult', function(req, res, next) {
+	mult.get(req, res);
+	next();
+});
+app.use('/fibonacci2', function(req, res, next) {
+	fibonacci2.get(req, res);
+	next();
+});
+app.use('/fibonacci', function(req, res, next) {
+	fibonacci.get(req, res);
+	next();
+});
+app.use('/factorial', function(req, res, next) {
+	factorial.get(req, res);
+	next();
+});
+app.use('/square', function(req, res, next) {
+	square.get(req, res);
+	next();
+});
 
+let server = http.createServer(app);
 server.listen(Port);
 console.log('listening to http://localhost:8124');
